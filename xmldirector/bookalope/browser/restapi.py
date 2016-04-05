0ab71855808e022cf118bc49114b3e548a9528af
Andreas JungÂ <info@zopyx.com>
2016-04-05 13:54:06 +0200

    added rest API

---
+++ xmldirector/bookalope/browser/restapi.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+################################################################
+# xmldirector.bookalope
+# (C) 2016,  Andreas Jung, www.zopyx.com, Tuebingen, Germany
+################################################################
+
+from zopyx.plone.persistentlogger.logger import IPersistentLogger
+
+from xmldirector.plonecore.browser.restapi import BaseService
+from xmldirector.plonecore.browser.restapi import decode_json_payload
+
+from xmldirector.bookalope.browser.api import convert_bookalope
+
+
+class BookalopeConversionError(Exception):
+    """ A generic Bookalope error """
+
+
+class api_convert_bookalope(BaseService):
+
+    def _render(self):
+
+        IPersistentLogger(self.context).log('bookalope-convert')
+        payload = decode_json_payload(self.request)
+
+        print payload
+        convert_bookalope(self.context, **payload)
+        
