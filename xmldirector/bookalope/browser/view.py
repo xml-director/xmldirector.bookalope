0bc16bd91c186eec81a65d5db4a6d7092ec7d001
Andreas JungÂ <info@zopyx.com>
2016-04-06 15:16:30 +0200

    updated

--- xmldirector/bookalope/browser/view.py
+++ xmldirector/bookalope/browser/view.py
@@ -5,10 +5,67 @@
 # (C) 2016,  Andreas Jung, www.zopyx.com, Tuebingen, Germany
 ################################################################
 
-
+import os
 from Products.Five.browser import BrowserView
 
 
 class Bookalope(BrowserView):
 
-    pass
+
+    def upload_source(self):
+
+        source = self.request.get('source')
+        if not source:
+            raise ValueError('No file uploaded')
+
+        basename, ext = os.path.splitext(os.path.basename(source.filename))
+        if not ext.lower() in ('.docx', '.icml'):
+            raise ValueError('Uploaded files must be either ICML or DOCX')
+        filename = unicode(basename, 'utf8', 'ignore').encode('ascii', 'ignore')
+        filename = 'src/{}{}'.format(filename, ext.lower())
+        handle = self.context.get_handle()
+        if not handle.exists('src'):
+            handle.makedir('src')
+
+        with handle.open(filename, 'wb') as fp:
+            source.seek(0)
+            fp.write(source.read())
+
+        self.context.plone_utils.addPortalMessage(u'Upload completed')
+        self.request.response.redirect(self.context.absolute_url() + '/@@xmldirector-bookalope')
+
+    def upload_cover(self):
+
+        source = self.request.get('source')
+        if not source:
+            raise ValueError('No file uploaded')
+
+        basename, ext = os.path.splitext(os.path.basename(source.filename))
+        if not ext.lower() in ('.jpg', '.png', '.gif'):
+            raise ValueError('Uploaded files must be either GIF, JPG or PNG')
+        filename = unicode(basename, 'utf8', 'ignore').encode('ascii', 'ignore')
+        filename = 'src/{}{}'.format(filename, ext.lower())
+        handle = self.context.get_handle()
+        if not handle.exists('src'):
+            handle.makedir('src')
+
+        with handle.open(filename, 'wb') as fp:
+            source.seek(0)
+            fp.write(source.read())
+
+        self.context.plone_utils.addPortalMessage(u'Upload completed')
+        self.request.response.redirect(self.context.absolute_url() + '/@@xmldirector-bookalope')
+
+    def get_cover_files(self):
+        handle = self.context.get_handle()
+        if not handle.exists('src'):
+            return ()
+        return [ 'src/{}'.format(name) for name in handle.listdir('src') if name.endswith(('.png', '.jpg', '.gif'))]
+
+    def get_source_files(self):
+        handle = self.context.get_handle()
+        if not handle.exists('src'):
+            return ()
+        return [ 'src/{}'.format(name) for name in handle.listdir('src') if name.endswith(('.icml', '.docx'))]
+
+
