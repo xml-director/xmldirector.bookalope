c42ff445f31a26f1e6cefe05d76d9cef729440d1
Andreas JungÂ <info@zopyx.com>
2016-04-06 14:33:54 +0200

    updated

---
+++ xmldirector/bookalope/browser/view.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+################################################################
+# xmldirector.bookalope
+# (C) 2016,  Andreas Jung, www.zopyx.com, Tuebingen, Germany
+################################################################
+
+
+from Products.Five.browser import BrowserView
+
+
+class Bookalope(BrowserView):
+
+    pass
